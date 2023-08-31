3ab90edba86e508590cc004a4293a763e7028500
Hamidreza <hamidreza@Hamidrezas-MacBook-Air.local>
2023-08-31 20:58:52 +0330

    add test, handle errors

--- golang/app1/app1.go
+++ golang/app1/app1.go
@@ -3,14 +3,47 @@ package main
 import (
 	"fmt"
 
-	"rsc.io/quote"
+	"log"
 
+	//"example/app1_mod"
 	"example/app2"
+	//"app1_mod"
 )
 
 func main() {
-	fmt.Println(quote.Go())
+	// fmt.Println(quote.Go())
 
-	message := app2.Hello("Gladys")
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("app2: ")
+	log.SetFlags(0)
+
+	// Request a greeting message.
+	//message, err := app2.Hello("")
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	message, err := app2.Hello("Gladys")
+	message2, err2 := Hello2("dfdsd")
+	// If an error was returned, print it to the console and
+	// exit the program.
+
+	if err2 != nil {
+		log.Fatal(err2)
+	}
+
+	// If no error was returned, print the returned message
+	// to the console.
 	fmt.Println(message)
+	fmt.Println(message2)
+
+	// Request greeting messages for the names.
+	messages, err := app2.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
 }
--- golang/app1/go.mod
+++ golang/app1/go.mod
@@ -2,14 +2,6 @@ module example/app1
 
 go 1.20
 
-require (
-	example/app2 v0.0.0-00010101000000-000000000000
-	rsc.io/quote v1.5.2
-)
-
-require (
-	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
-	rsc.io/sampler v1.3.0 // indirect
-)
+require example/app2 v0.0.0-00010101000000-000000000000
 
 replace example/app2 => ../app2
